57d877bf18b0c208d1981e23c651f0e00ef47171
Mel Fabella <mf820am@gmail.com>
2020-12-07 22:26:57 -0800

    started on songs

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,53 @@
+class SongsController < ApplicationController
+  before_action :set_billboard
+  before_action :set_song, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @songs = @billboard.songs 
+  end
+
+  def show
+  end
+
+  def new
+    @artists = Billboard.all - @billboard.artists
+    @song = @billboard.songs.new 
+  end
+
+  def create
+    @song = @billboard.songs.new(song_params)
+    if @song.save
+       redirect_to billboard_songs_path(@billboard)
+    else
+      render :new 
+    end 
+  end
+
+  def edit
+    @artists = Billboard.all - @billboard.artists
+  end
+
+  def update
+    if @song.updat(song_params)
+      redirect_to billboard_songs_path(@billboard)
+    else
+      render :edit
+    end 
+  end
+
+  def destroy
+    @song.destroy
+    redirect_to billboard_songs_path(@billboard)
+  end 
+
+  private
+    def set_billboard
+      @billboard = Billboard.find(params[:billboard_id])
+    end
+    def set_song
+      @enrollment =@billboard.songs.find(params[:id])
+
+    def song_params 
+      params.require(:song).permit(:title, :album, :genre, :length, :artist_id)
+    end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'billboards#index'
 
   resources :billboards
-  resources :artists
+  resources :artists do
+    resources :songs
+  end
 end
