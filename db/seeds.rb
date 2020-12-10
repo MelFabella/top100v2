cebfd0ed4bfba706a3546dd59902c41158e1d148
Mel Fabella <mf820am@gmail.com>
2020-12-09 23:58:41 -0800

    fixing views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,21 @@
 10.times do
-  artist = Artist.create(
-    name: Faker::Music.band
+  billboard = Billboard.create(
+    title: Faker::Music.genre,
+    details: Faker::Movies::StarWars.quote
   )
 
   5.times do
-    billboard = Billboard.create(
-      title: Faker::Hipster.word,
-      details: Faker::Book.genre 
-    )
-
-    Song.create(
-      artist_id: artist.id,
-      billboard_id: billboard.id
-    )
-  end
-end
-
-7.times do
-  billboard = Billboard.create(
-    title: Faker::Music.genre
-  )
-  3.times do
     artist = Artist.create(
-      name: Faker::Music.band
-      rank: Faker::Number.number(digits: 100)
-      numAlbs: Faker::Number.number(digits: 10)
+      name: Faker::Music.band,
+      rank: Faker::Number.number(digits: 100),
+      numAlbs: Faker::Number.number(digits: 10)  
     )
+
     Song.create(
       billboard_id: billboard.id,
-      artist_id: artist.id,
-      #genre: genre.id,
+      artist_id: artist.id
     )
   end
 end
-puts 'Data Seeded'
+
+puts 'Data seeded'
